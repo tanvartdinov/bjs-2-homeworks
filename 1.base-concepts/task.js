7c57075794ebd4f3a12f9f43d36b46a557ce4338
Timur Anvartdinov <t.anvartdinov@life-stream.tv>
2023-10-11 21:57:40 +0300

    aaaa

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,18 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
+  const arr = [];
+  const d = b * b - 4 * a * c;
+  if (d > 0) {
+    const x1 = (-b + Math.sqrt(d) ) / (2*a);
+    const x2 = (-b - Math.sqrt(d) ) / (2*a);
+    arr.push(x1);
+    arr.push(x2);
+  } else if (d === 0 ) {
+    const x = -b/(2*a);
+    arr.push(x);
+  }
+
   return arr;
 }
 
